0836f71437acf7014ab1be9ffead6c0acb44018f
Henrique Fadoni <henriquefadonii@gmail.com>
2019-01-20 13:13:46 -0200

    Users.js basic set up

---
+++ src/container/User.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Pizza extends Comment {
+    render () {
+        return (
+            <div>
+                <h1>The Users</h1>
+                <p>They ate the pizza</p>
+            </div>
+        );
+    }
+}
+
+export default Pizza;
